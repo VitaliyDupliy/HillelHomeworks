65b172d160ab0e984d1c66abefc62e889579db5d
Dupliy Vitaliy <gidrohlorid9@gmail.com>
2023-05-27 21:35:18 +0300

    hw15

---
+++ src/ua/hillel/hw15/App.java
@@ -0,0 +1,43 @@
+package ua.hillel.hw15;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class App {
+
+	public static void main(String[] args) {
+		Product p1 = new Product("Book", 280, true);
+		Product p2 = new Product("Toy", 15, true);
+		Product p3 = new Product("Food", 350, false);
+		Product p4 = new Product("Book", 50, true);
+		Product p5 = new Product("Dress", 150, false);
+		Product p6 = new Product("Dress", 300, true);
+		Product p7 = new Product("Food", 100, true);
+		Product p8 = new Product("Dress", 60, true);
+		Product p9 = new Product("Book", 51, true);
+		Product p10 = new Product("Toy", 50, true);
+		
+	
+		List<Product> list = new ArrayList<>();
+		list.add(p1);
+		list.add(p2);
+		list.add(p3);
+		list.add(p4);
+		list.add(p5);		
+		list.add(p6);
+		list.add(p7);
+		list.add(p8);
+		list.add(p9);
+		list.add(p10);
+		
+//		System.out.println(Product.totalCostProducts(list));
+//		System.out.println(Product.groupProd250(list));
+//		System.out.println(Product.discountProd(list));
+//		System.out.println(Product.cheapestProd(list));
+		System.out.println(Product.find3LastProd(list));
+//		System.out.println(Product.totalCostProducts(list, "Dress"));
+//		System.out.println(Product.groupProduct (list));
+
+	}
+
+}
---
+++ src/ua/hillel/hw15/Product.java
@@ -0,0 +1,145 @@
+package ua.hillel.hw15;
+
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Map;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.stream.Collectors;
+ 
+
+public class Product {
+
+	private String tipe;
+	private double price;
+	private boolean discountAble;
+	static final LocalDate dataOFAdding = LocalDate.now();
+	
+	
+
+	public Product(String tipe, double price, boolean discountAble) {
+		this.tipe = tipe;
+		this.price = price;
+		this.discountAble = discountAble;
+		
+	}
+
+	/*
+	 * 1.2 Реалізувати метод отримання всіх продуктів у вигляді списку, категорія яких еквівалентна “Book”
+	 *  та ціна більш ніж 250.
+	 */
+
+	public static List<Product> groupProd250(List<Product> prod) {
+		return prod.stream().filter(p -> p.getTipe() == "Book" & p.getPrice() > 250)
+				.collect(Collectors.toList());
+	}
+
+	/*
+	 * 2.2 Реалізувати метод отримання всіх продуктів як списку, категорія яких еквівалентна “Book”
+	 *  і з можливістю застосування знижки. Фінальний список повинен містити продукти з застосованою 
+	 *  знижкою 10%. Так, якщо Продукт A був з ціною 1.0 USD, то його фінальна ціна залишатиметься 0.9 USD
+	 */
+
+	public static List<Product> discountProd(List<Product> prod) {
+		return prod.stream()
+				.filter(p -> p.getTipe() == "Book" & p.isDiscountAble())
+				.map(p -> {
+			p.setPrice(p.getPrice() * 0.9);
+			return p;
+		})
+				.collect(Collectors.toList());
+	}
+	
+	
+	//3.2 Реалізувати метод отримання найдешевшого продукту з категорії “Book”
+	
+	public static Product cheapestProd (List<Product> prod) {
+						 
+		return  prod.stream()
+				.filter(p -> p.getTipe() == "Book")
+				.min((a,b) -> Double.compare(a.getPrice(), b.getPrice())).get();
+		
+		}
+		 
+		//4.2 Реалізувати метод отримання трьох останніх доданих продуктів
+	
+	public static List<Product> find3LastProd (List<Product> prod) {
+		
+	    return prod.subList((prod.size() - 3) , prod.size());
+	}
+	
+	/*
+	 * 5.2 Реалізувати метод калькуляції загальної вартості продуктів, які відповідають наступним критеріям:
+
+- продукт додано протягом поточного року
+
+- продукт має тип “Book”
+
+- ціна продукту не перевищує 75
+	 */
+	
+	public static double totalCostProducts (List<Product> prod, String tipe ) {
+		return prod.stream()
+				.filter(p -> p.getDataOFAdding().getYear() == LocalDate.now().getYear()
+				& (p.getTipe() == tipe) & (p.getPrice() <= 75))
+				.map(p -> p.getPrice()).reduce((a,p) -> a + p).get();
+					 				
+	}
+	
+	/*
+	 * 6.2 Реалізувати метод групування об'єктів за типом продукту. Таким чином результатом 
+	 * виконання методу буде тип даних “Словник”, що зберігає пару ключ-значення: 
+	 * {тип: список_продуктів}
+	 */
+
+	public static Map<String, ArrayList<Product>> groupProduct (List<Product> prod) {
+		Map<String, ArrayList<Product>> tipesMap = new HashMap<>();
+		prod.stream().forEach(p -> {if(tipesMap.containsKey(p.getTipe())) {
+			
+			tipesMap.get(p.getTipe()).add(p);
+		} else {
+			tipesMap.putIfAbsent(p.getTipe(), new ArrayList<Product>());
+			tipesMap.get(p.getTipe()).add(p);
+		}
+				
+				});
+		
+		return tipesMap;
+		
+	}
+	
+	
+	public boolean isDiscountAble() {
+		return discountAble;
+	}
+
+
+
+	public LocalDate getDataOFAdding() {
+		return dataOFAdding;
+	}
+
+
+
+	public String getTipe() {
+		return tipe;
+	}
+
+
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+	@Override
+	public String toString() {
+		return "Product [tipe=" + tipe + ", price=" + price + ", discountAble=" + discountAble 
+				+ " dataOFAdding " + LocalDate.now() +"]";
+	}
+
+}
